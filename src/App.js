7bd5ed191d95e9d92f0fb3d76cd16fcf30f127a7
Elena Granados <egranados@j2interactive.com>
2023-06-05 11:26:46 -0600

    implement use state; current count

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import SubscriberForm from "./SubscriberForm"
+import ProfileEdit from './ProfileEdit';
 
 function App() {
+  const [subscribed, setSubscribed] = useState(false);
+  const [subscribedCount, setSubscribedCount] = useState(0);
+  const [alerts, setAlerts] = useState(false);
+  const [userId, setUserID] = useState(1);
+
+  const userIds = [1, 2, 3, 4];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section>
+      <p>Please click to subscribe to my updates!</p>
+      <p>Subscriber Count: {subscribedCount}</p>
+      <button
+        onClick={() => {
+          setSubscribed(!subscribed);
+          setSubscribedCount((c) => c + 1);
+          setSubscribedCount((c) => c + 1);
+          if (!alerts) setAlerts(true);
+        }}
+      >
+        {subscribed ? "Unsubscribe" : "Subscribe"}
+      </button>
+      <button onClick={() => setAlerts(!alerts)}>
+        {alerts ? "Stop Email Alerts" : "Get Email Alerts"}
+      </button>
+      <SubscriberForm />
+
+      <div className="App">
+      {userIds.map((id) => (
+        <button key={id} onClick={() => setUserID(id)}>
+          User ID {id}
+        </button>
+      ))}
+      <h2>User ID {userId}</h2>
+      <ProfileEdit userID={userId} />
     </div>
+    </section>
   );
 }
 
